09742178121dbddaef28a0051a719e4c4800f8b5
Meg Galvez <meg@oakmaine.com>
2019-02-22 16:23:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,12 @@
-def prime?(i)
-  range = (-1..100000)
-  range.select(&:odd?) << prime_array
-  
-  
-  
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| 
+      
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
